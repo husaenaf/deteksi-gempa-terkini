9adb7f6efbea64d24d6d8560707b7a5ce74c330d
Husaen al Fatah <husaenaf26@gmail.com>
2023-09-09 14:11:58 +0700

    masih error

---
+++ main.py
@@ -0,0 +1,43 @@
+"""
+aplikasi deteksi gempa terkini
+MODULARISASI DENGAN FUNCTION
+"""
+
+
+def ekstraksi_data():
+
+    """
+    Tanggal: 09 September 2023, 12:02:51 WIB
+    Magnitudo: 5.0
+    Kedalaman: 210 km
+    Lokasi: 7.62 LS - 128.54 BT
+    Pusat Gempa: 101 km TimurLaut MALUKUBRTDAYA
+    Keterangan: tidak berpotensi TSUNAMI
+    :return:
+    """
+
+    hasil = dict
+    hasil['tanggal'] = '9 September 2023'
+    hasil['waktu'] = '12:02:51 WIB'
+    hasil['Magnitudo'] = 4.0
+    hasil['lokasi'] = {'ls': 7.62, 'bt': 128.54}
+    hasil['pusat'] = '101 km TimurLaut MALUKUBRTDAYA'
+    hasil['keterangan'] = 'tidak berpotensi TSUNAMI'
+
+    return hasil
+
+
+def tampilkan_data(result):
+    print('Gempa terakhir berdasarkan BMKG')
+    print(f"tanggal {result['tanggal']}")
+    print(f"waktu {result['waktu']}")
+    print(f"magnitudo {result['magnitudo']}")
+    print(f"lokasi LS={result['lokasi']['ls']}, BT={result['lokasi']['bt']}")
+    print(f"pusat {result['pusat']}")
+    print(f"keterangan {result['keterangan']}")
+
+
+if __name__ == '__main__':
+    print('Aplikasi Utama')
+    result = ekstraksi_data()
+    tampilkan_data(result)
